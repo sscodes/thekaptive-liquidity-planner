544242faf9a6988bde636aa7ac1d66843749a1dc
sscodes <sanketcrj@gmail.com>
2024-06-05 14:25:40 +0530

    added server

--- server/server.ts
+++ server/server.ts
@@ -1,31 +1,20 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const express = require('express');
+const fs = require('fs'); // Node.js file system module
+const app = express();
+var cors = require('cors');
+const PORT = process.env.PORT || 3000;
 
-const server = jsonServer.create()
+// Read data from db.json
+const jsonData = JSON.parse(fs.readFileSync('db.json', 'utf8'));
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+app.use(cors());
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+// Define a route to serve your JSON data
+app.get('/api/data', (_req, res) => {
+  res.json(jsonData);
+});
 
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
-
-// Export the Server API
-module.exports = server
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
